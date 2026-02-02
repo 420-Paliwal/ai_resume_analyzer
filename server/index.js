e0a9f974e2a330dde7d08707dc97c8b9d4c13373
420-Paliwal <harshpaliwal0420@gmail.com>
2026-02-02 10:17:06 +0530

    router bug fix

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const cors = require("cors")
+const multer  = require('multer')
+const upload = multer()
+
 
 const app = express()
 const port = 3000
 
 app.get('/', (req,res)=>{
-    res.status(200).json({"message" : "Home Page"})
+    return res.status(200).json({"message" : "Home Page"})
+})
+
+app.get('/test', (req, res)=>{
+    return res. status(200).json({"message" : "Test Page"})
 })
 
 app.listen(port, ()=>{
