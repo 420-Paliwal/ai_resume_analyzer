373e466fb9df81313519300ec94db65d0f4c0e50
420-Paliwal <harshpaliwal0420@gmail.com>
2026-02-02 10:45:20 +0530

    post upload route for resume

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
-const express = require("express")
-const cors = require("cors")
-const multer  = require('multer')
-const upload = multer()
+const express = require("express");
+const cors = require("cors");
+const multer  = require('multer');
+const storage = multer.memoryStorage();
+const pdfParser = require("pdf-parser")
 
+const upload = multer({
+    storage : storage
+});
 
 const app = express()
 const port = 3000
@@ -14,6 +18,25 @@ app.get('/', (req,res)=>{
 app.get('/test', (req, res)=>{
     return res. status(200).json({"message" : "Test Page"})
 })
+
+app.post('/upload', 
+    upload.single("resume"),
+    async (req, res)=>{
+        try{
+            if(!req.file){
+                return res.status(400).json({
+                    error : "NO file uploaded"
+                })
+            }
+            const data = await pdfParser(req.file.buffer)
+            console.log(data.text);
+            res.status(200).json({ text : data.text})
+        }catch(err){
+            return res.status(400).json({
+                error : "Failed to parse PDF"})
+        }
+})
+
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
